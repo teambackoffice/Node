6009cf2716812eb8ea38ece2d82c745df78bd98c
sammish <sammish.thundiyil@gmail.com>
2025-03-20 22:32:17 +0530

    first commit

---
+++ node/node/doctype/os/test_os.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, sammish.thundiyil@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOS(FrappeTestCase):
+	pass
---
+++ node/node/doctype/os_version/os_version.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, sammish.thundiyil@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("OS Version", {
+// 	refresh(frm) {
+
+// 	},
+// });
